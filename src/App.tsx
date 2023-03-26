df97cd20b2e6cfe3b315eee467223415a62889e4
Yash Garg <thisisyashgarg@gmail.com>
2023-03-26 22:53:25 +0530

    dice changing color on click feature added

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,63 @@
 import React, { useEffect, useState } from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
+
+type DiceObject = {
+  value: number;
+  isHeld: boolean;
+  id: string;
+};
 
 export default function App() {
-  const [dice, setDice] = useState([]);
-
-  function createAnArrayOf10RandomNumbers(): void {
-    const array: number[] = [];
-    for (let i = 0; i < 10; i++) {
-      array.push(Math.floor(Math.random() * 6) + 1);
-    }
-    setDice(array);
-  }
+  const [dice, setDice] = useState<Array<DiceObject>>([]);
 
   useEffect(() => {
-    createAnArrayOf10RandomNumbers();
+    allNewDie();
   }, []);
+
+  function createAnArrayOf10RandomDice(): DiceObject[] {
+    const array: DiceObject[] = [];
+    for (let i = 0; i < 10; i++) {
+      array.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return array;
+  }
+
+  function allNewDie() {
+    setDice(createAnArrayOf10RandomDice());
+  }
+
+  function changeColor(id: string) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+    console.log(id);
+  }
 
   return (
     <main className=" h-[90vh] mr-9 m-10 bg-slate-200 rounded-lg flex flex-col justify-center items-center">
       <div className="grid grid-rows-2 grid-cols-5 gap-12 mx-10 p-5">
-        {dice.map((number: number, index) => {
-          return <Die value={number} key={index} />;
+        {dice.map((dice: DiceObject) => {
+          return (
+            <Die
+              value={dice.value}
+              changeColor={() => changeColor(dice.id)}
+              isHeld={dice.isHeld}
+              key={dice.id}
+            />
+          );
         })}
       </div>
 
       <button
-        className="p-4 px-10 text-white rounded-md text-2xl m-4 bg-blue-700 tracking-wider hover:bg-blue-600"
-        onClick={createAnArrayOf10RandomNumbers}
+        className="p-4 px-10 text-white rounded-md text-2xl m-4 bg-[#5036FF] tracking-wider "
+        onClick={createAnArrayOf10RandomDice}
       >
         Roll
       </button>
--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-export default function Die({ value }) {
+export default function Die({ value, changeColor, isHeld }) {
   return (
-    <div className="w-16 h-16 shadow-md rounded-md flex justify-center items-center my-4">
+    <div
+      className={`w-16 h-16 shadow-md rounded-md flex justify-center items-center my-4 ${
+        isHeld ? "bg-[#59E391]" : "bg-white"
+      }`}
+      onClick={changeColor}
+    >
       <h2 className="font-bold text-4xl ">{value}</h2>
     </div>
   );
