5fbc8d3b605b7564b90fdcb96d34cca7ce0e53dd
Yash Garg <thisisyashgarg@gmail.com>
2023-03-26 22:12:47 +0530

    added a roll button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Die from "./components/Die";
 
 export default function App() {
-  const [dice, newDice] = useState(allNewDice());
+  const [dice, setDice] = useState([]);
 
-  function allNewDice(): number[] {
+  function createAnArrayOf10RandomNumbers(): void {
     const array: number[] = [];
     for (let i = 0; i < 10; i++) {
       array.push(Math.floor(Math.random() * 6) + 1);
     }
-    return array;
+    setDice(array);
   }
+
+  useEffect(() => {
+    createAnArrayOf10RandomNumbers();
+  }, []);
 
   return (
     <main className=" h-[90vh] mr-9 m-10 bg-slate-200 rounded-lg flex flex-col justify-center items-center">
       <div className="grid grid-rows-2 grid-cols-5 gap-12 mx-10 p-5">
-        {dice.map((number, index) => {
+        {dice.map((number: number, index) => {
           return <Die value={number} key={index} />;
         })}
       </div>
+
+      <button
+        className="p-4 px-10 text-white rounded-md text-2xl m-4 bg-blue-700 tracking-wider hover:bg-blue-600"
+        onClick={createAnArrayOf10RandomNumbers}
+      >
+        Roll
+      </button>
     </main>
   );
 }
--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Die(props) {
+export default function Die({ value }) {
   return (
     <div className="w-16 h-16 shadow-md rounded-md flex justify-center items-center my-4">
-      <h2 className="font-bold text-4xl ">{props.value}</h2>
+      <h2 className="font-bold text-4xl ">{value}</h2>
     </div>
   );
 }
