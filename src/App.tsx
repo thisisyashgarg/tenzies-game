53a09a6adaada51054f677bc7fa467e5342d514a
Yash Garg <thisisyashgarg@gmail.com>
2023-03-27 16:58:09 +0530

    solved responsiveness problem

--- src/App.tsx
+++ src/App.tsx
@@ -48,60 +48,28 @@ export default function App() {
   }
 
   return (
-    // <main className=" h-[90vh] mr-9 m-10 bg-slate-200 rounded-lg flex flex-col justify-center items-center">
-    //   {tenzies && <ConfettiEffect />}
-    //   <h1 className="text-6xl font-bold mb-4">Tenzies</h1>
-    //   <p className="text-2xl max-w-md mb-8 text-center">
-    //     Roll until all dice are the same. Click each die to freeze it at its
-    //     current value between rolls.
-    //   </p>
-    //   <div className="grid grid-rows-2 grid-cols-5 gap-12 mx-10 p-5">
-    //     {dice.map((dice: DiceObject) => {
-    //       return (
-    //         <Die
-    //           value={dice.value}
-    //           changeColor={() => changeColor(dice.id)}
-    //           isHeld={dice.isHeld}
-    //           key={dice.id}
-    //           tenzies={tenzies}
-    //         />
-    //       );
-    //     })}
-    //   </div>
-
-    //   <button
-    //     className="p-4 px-10 text-white rounded-md text-2xl m-4 mt-10 bg-[#5036FF] tracking-wider "
-    //     onClick={rollDice}
-    //   >
-    //     {tenzies ? "New Game" : "Roll"}
-    //   </button>
-    // </main>
-
-    <main className="h-screen sm:h-screen  rounded-lg flex flex-col justify-center items-center overflow-x-hidden my-4 sm:my-8">
+    <main className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-8 lg:px-16">
       {tenzies && <ConfettiEffect />}
-      <h1 className="text-4xl sm:text-6xl font-bold mb-4 mt-8 sm:mt-0 text-center">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mt-4 sm:mt-8 text-center">
         Tenzies
       </h1>
-      <p className="text-lg sm:text-2xl max-w-md mb-8 text-center">
+      <p className="text-lg sm:text-xl md:text-2xl max-w-md mt-4 sm:mt-6 mb-8 text-center">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
-      <div className="grid grid-cols-2 sm:grid-cols-5 gap-4 sm:gap-12 mx-4 sm:mx-10 p-2 sm:p-5">
-        {dice.map((dice: DiceObject) => {
-          return (
-            <Die
-              value={dice.value}
-              changeColor={() => changeColor(dice.id)}
-              isHeld={dice.isHeld}
-              key={dice.id}
-              tenzies={tenzies}
-            />
-          );
-        })}
+      <div className="grid grid-cols-2 sm:grid-cols-5 gap-4 sm:gap-8 md:gap-12 p-2 sm:p-5">
+        {dice.map((dice) => (
+          <Die
+            key={dice.id}
+            value={dice.value}
+            isHeld={dice.isHeld}
+            changeColor={() => changeColor(dice.id)}
+            tenzies={tenzies}
+          />
+        ))}
       </div>
-
       <button
-        className="p-2 sm:p-4 px-6 sm:px-10 text-white rounded-md text-lg sm:text-2xl m-2 sm:m-4 mt-8 sm:mt-10 bg-[#5036FF] tracking-wider"
+        className="px-4 sm:px-6 md:px-8 py-2 sm:py-4 md:py-6 text-lg sm:text-xl md:text-2xl text-white font-bold rounded-md bg-[#5036FF]  mt-8 sm:mt-10 tracking-wider"
         onClick={rollDice}
       >
         {tenzies ? "New Game" : "Roll"}
